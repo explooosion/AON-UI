b42ea56cd386abc9371cca19e5d889e4f9f4b44f
Robby <ta7382@gmail.com>
2018-06-01 06:19:48 +0800

    Update ue4 lib

---
+++ src/plugin/UELib.js
@@ -0,0 +1,47 @@
+export default class UELib {
+
+  constructor(actions) {
+
+    this.actions = actions
+
+    this.ue4 = null
+
+    if (typeof ue === 'object' && typeof ue.interface === 'object') {
+
+      if (typeof ue.interface.broadcast != 'function') return
+
+      try {
+
+        ue.interface = {}
+
+        ue.interface.broadcast = (name, data) => {
+          if (typeof data !== 'undefined')
+            ue.interface.broadcast(name, JSON.stringify(data))
+          else
+            ue.interface.broadcast(name, '')
+        }
+
+        this.ue4 = ue.interface.broadcast
+
+        ue.interface.setFPS = (fps) => {
+          this.FPS = fps.toFixed(1)
+        }
+
+        // simple way
+        ue.interface.setCurrentHero = actions.player.update
+
+        // ue.interface.setProgress = this.setProgress.bind(this)
+        // ue.interface.hideProgress = this.hideProgress.bind(this)
+        // ue.interface.setCurrentHero = this.setCurrentHero.bind(this)
+
+      } catch (e) { alert(e) }
+
+    } // end if
+
+  }
+
+  // setCurrentHero(val) {
+  //   this.actions.player.update(val)
+  // }
+
+}
